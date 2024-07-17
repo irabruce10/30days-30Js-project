6a029c1a4418f34ae73ad3f335e2e7439870655f
Bruce <irabruce10@gmail.com>
2024-07-17 14:11:09 +0200

    add js

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -1,1 +1,19 @@
+const key = "5ea6d2d405d85b936245b654206c31fb";
 
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=linz";
+
+async function getWeatherData() {
+  const response = await fetch(url + `&appid=${key}`);
+  const data = await response.json();
+  console.log(data);
+
+  //   if (response.ok) {
+  //     return data;
+  //     console.log(data);
+  //   } else {
+  //     throw new Error(`HTTP error! status: ${response.status}`);
+  //   }
+}
+
+getWeatherData();
