feb910bc2c499d02569100fe5df1369f3e104cc3
Bruce <irabruce10@gmail.com>
2024-07-18 12:49:16 +0200

    todo App

--- 02TodoApp/script.js
+++ 02TodoApp/script.js
@@ -2,6 +2,14 @@ const form = document.getElementById("todoForm");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const element = document.querySelector(".todoInput").value;
+  const element = document.querySelector(".todoInput");
   console.log(element);
+
+  if (element.value === " ") {
+    alert("Please add something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = element.value;
+    document.querySelector(".todoList").appendChild(li);
+  }
 });
