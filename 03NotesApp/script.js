e57642240b7605e34799471a85d54eb3713500cb
Bruce <irabruce10@gmail.com>
2024-07-21 17:14:08 +0200

    notes App

---
+++ 03NotesApp/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".btn");
+const txt = document.querySelector(".input-note");
+console.log(btn);
+
+btn.addEventListener("click", () => {
+
+    
+  console.log("Button clicked");
+});
