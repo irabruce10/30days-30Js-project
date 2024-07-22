5687b24adebac839a93dae4af0d42cc68ec2cd67
Bruce <irabruce10@gmail.com>
2024-07-22 18:40:01 +0200

    Age Calculator App

--- 04AgeCalculator/script.js
+++ 04AgeCalculator/script.js
@@ -2,17 +2,6 @@ let btn = document.querySelector(".btn");
 
 let iptValue = document.getElementById("date");
 
-console.log(iptValue);
-
-let g = document.getElementById("g");
-
-btn.addEventListener("click", () => {
-  console.log(iptValue.value);
-  console.log(g.value);
-});
-
-console.log(g.value);
-
 // btn.addEventListener("click", () => {
 //   let bday = new Date(iptValue.value);
 
@@ -23,25 +12,25 @@ console.log(g.value);
 //   console.log(bday);
 // });
 
-// function calcAge() {
-//   let bday = new Date(iptValue.value);
-//   console.log(bday);
+function calcAge() {
+  let bday = new Date(iptValue.value);
+  console.log(bday);
 
-//   let d1 = bday.getDate();
-//   let m1 = bday.getMonth() + 1;
-//   let y1 = bday.getFullYear();
+  let d1 = bday.getDate();
+  let m1 = bday.getMonth() + 1;
+  let y1 = bday.getFullYear();
 
-//   let today = new Date();
+  let today = new Date();
 
-//   let d2 = today.getDate();
-//   let m2 = today.getMonth() + 1;
-//   let y2 = today.getFullYear();
+  let d2 = today.getDate();
+  let m2 = today.getMonth() + 1;
+  let y2 = today.getFullYear();
 
-//   let y3 = y2 - y1;
+  let y3 = y2 - y1;
 
-//   console.log(y3);
+  console.log(y3);
 
-//   console.log(today);
-// }
+  console.log(today);
+}
 
-// btn.addEventListener("click", calcAge);
+btn.addEventListener("click", calcAge);
